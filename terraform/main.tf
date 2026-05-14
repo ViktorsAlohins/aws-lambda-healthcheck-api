ca8c26b98851324cc5b5b4382f07d0b2b78d98aa
Viktors Alohins <viktors.alohins96@gmail.com>
2026-05-14 18:05:33 +0300

    gateway connect

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,7 @@ terraform {
 
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "~> 6.0"
     }
   }
@@ -105,4 +105,54 @@ resource "aws_lambda_function" "health_check" {
   depends_on = [
     aws_cloudwatch_log_group.lambda
   ]
+}
+
+resource "aws_apigatewayv2_api" "health_check" {
+  name          = "${var.env}-health-check-api"
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_integration" "lambda" {
+  api_id                 = aws_apigatewayv2_api.health_check.id
+  integration_type       = "AWS_PROXY"
+  integration_uri        = aws_lambda_function.health_check.invoke_arn
+  payload_format_version = "2.0"
+}
+
+resource "aws_apigatewayv2_integration" "lambda" {
+  api_id                 = aws_apigatewayv2_api.health_check.id
+  integration_type       = "AWS_PROXY"
+  integration_uri        = aws_lambda_function.health_check.invoke_arn
+  payload_format_version = "2.0"
+}
+
+resource "aws_apigatewayv2_route" "get_health" {
+  api_id    = aws_apigatewayv2_api.health_check.id
+  route_key = "GET /health"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda.id}"
+}
+
+resource "aws_apigatewayv2_route" "post_health" {
+  api_id    = aws_apigatewayv2_api.health_check.id
+  route_key = "POST /health"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda.id}"
+}
+
+resource "aws_apigatewayv2_stage" "default" {
+  api_id      = aws_apigatewayv2_api.health_check.id
+  name        = "$default"
+  auto_deploy = true
+
+  default_route_settings {
+    throttling_rate_limit  = var.api_rate_limit
+    throttling_burst_limit = var.api_burst_limit
+  }
+}
+
+resource "aws_lambda_permission" "allow_api_gateway" {
+  statement_id  = "${var.env}-allow-api-gateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.health_check.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.health_check.execution_arn}/*/*"
 }
