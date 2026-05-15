474d5d1b3a8b26d58f6d8de65c6762d8f1cc72f1
Viktors Alohins <viktors.alohins96@gmail.com>
2026-05-15 22:49:36 +0300

    add s3 remote backend for terraform state

--- bootstrap/main.tf
+++ bootstrap/main.tf
@@ -13,6 +13,58 @@ provider "aws" {
   region = var.aws_region
 }
 
+data "aws_caller_identity" "current" {}
+
+locals {
+  tfstate_bucket = "${data.aws_caller_identity.current.account_id}-tf-state"
+}
+
+resource "aws_s3_bucket" "tfstate" {
+  bucket = local.tfstate_bucket
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_s3_bucket_versioning" "tfstate" {
+  bucket = aws_s3_bucket.tfstate.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "tfstate" {
+  bucket = aws_s3_bucket.tfstate.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "tfstate" {
+  bucket = aws_s3_bucket.tfstate.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_dynamodb_table" "tfstate_lock" {
+  name         = "terraform-state-lock"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
 resource "aws_iam_openid_connect_provider" "github" {
   url = "https://token.actions.githubusercontent.com"
 
@@ -93,8 +145,10 @@ data "aws_iam_policy_document" "staging_deploy_policy" {
     actions = [
       "dynamodb:CreateTable",
       "dynamodb:DeleteTable",
+      "dynamodb:DescribeContinuousBackups",
       "dynamodb:DescribeTable",
       "dynamodb:UpdateTable",
+      "dynamodb:UpdateContinuousBackups",
       "dynamodb:TagResource",
       "dynamodb:UntagResource",
       "dynamodb:ListTagsOfResource",
@@ -142,7 +196,14 @@ data "aws_iam_policy_document" "staging_deploy_policy" {
     effect = "Allow"
 
     actions = [
+      "logs:CreateLogDelivery",
+      "logs:DeleteLogDelivery",
       "logs:DescribeLogGroups",
+      "logs:DescribeResourcePolicies",
+      "logs:GetLogDelivery",
+      "logs:ListLogDeliveries",
+      "logs:PutResourcePolicy",
+      "logs:UpdateLogDelivery",
     ]
 
     resources = ["*"]
@@ -206,6 +267,41 @@ data "aws_iam_policy_document" "staging_deploy_policy" {
 
     resources = ["*"]
   }
+
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "s3:GetObject",
+      "s3:PutObject",
+      "s3:DeleteObject",
+    ]
+
+    resources = ["arn:aws:s3:::${local.tfstate_bucket}/staging/*"]
+  }
+
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "s3:ListBucket",
+      "s3:GetBucketVersioning",
+    ]
+
+    resources = ["arn:aws:s3:::${local.tfstate_bucket}"]
+  }
+
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "dynamodb:GetItem",
+      "dynamodb:PutItem",
+      "dynamodb:DeleteItem",
+    ]
+
+    resources = ["arn:aws:dynamodb:${var.aws_region}:*:table/terraform-state-lock"]
+  }
 }
 
 data "aws_iam_policy_document" "prod_deploy_policy" {
@@ -215,8 +311,10 @@ data "aws_iam_policy_document" "prod_deploy_policy" {
     actions = [
       "dynamodb:CreateTable",
       "dynamodb:DeleteTable",
+      "dynamodb:DescribeContinuousBackups",
       "dynamodb:DescribeTable",
       "dynamodb:UpdateTable",
+      "dynamodb:UpdateContinuousBackups",
       "dynamodb:TagResource",
       "dynamodb:UntagResource",
       "dynamodb:ListTagsOfResource",
@@ -264,7 +362,14 @@ data "aws_iam_policy_document" "prod_deploy_policy" {
     effect = "Allow"
 
     actions = [
+      "logs:CreateLogDelivery",
+      "logs:DeleteLogDelivery",
       "logs:DescribeLogGroups",
+      "logs:DescribeResourcePolicies",
+      "logs:GetLogDelivery",
+      "logs:ListLogDeliveries",
+      "logs:PutResourcePolicy",
+      "logs:UpdateLogDelivery",
     ]
 
     resources = ["*"]
@@ -302,6 +407,41 @@ data "aws_iam_policy_document" "prod_deploy_policy" {
     ]
 
     resources = ["*"]
+  }
+
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "s3:GetObject",
+      "s3:PutObject",
+      "s3:DeleteObject",
+    ]
+
+    resources = ["arn:aws:s3:::${local.tfstate_bucket}/prod/*"]
+  }
+
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "s3:ListBucket",
+      "s3:GetBucketVersioning",
+    ]
+
+    resources = ["arn:aws:s3:::${local.tfstate_bucket}"]
+  }
+
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "dynamodb:GetItem",
+      "dynamodb:PutItem",
+      "dynamodb:DeleteItem",
+    ]
+
+    resources = ["arn:aws:dynamodb:${var.aws_region}:*:table/terraform-state-lock"]
   }
 
   statement {
@@ -348,4 +488,8 @@ output "staging_deploy_role_arn" {
 
 output "prod_deploy_role_arn" {
   value = aws_iam_role.prod_deploy.arn
+}
+
+output "tfstate_bucket" {
+  value = aws_s3_bucket.tfstate.id
 }
